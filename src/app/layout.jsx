2e7272e89c82d16cf3dafe3c2862805b6241131c
Ismayel Alam <ismayelalam0@gmail.com>
2023-08-24 18:40:19 +0600

    add static assets and links

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,12 +7,32 @@ const inter = Roboto({ subsets: ["latin"], weight: ["500"] });
 export const metadata = {
   title: "Ismayel Alam",
   description:
-    "Ismayel Alam is a passionate and self-taught front-end web developer with a strong foundation in HTML, CSS(with multiple framework and library), and JavaScript(with multiple framework and library). Seeking opportunities to leverage my skills and creativity to build visually stunning and user-friendly websites.",
+    "Ismayel Alam is a passionate and self-taught front-end web developer with a strong foundation in HTML, CSS, and JavaScript. Seeking opportunities to leverage my skills and creativity to build visually stunning and user-friendly websites.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={inter.className}>
         <NavBar />
         {children}
--- src/layout/Intro.jsx
+++ src/layout/Intro.jsx
@@ -2,14 +2,14 @@ import { Nunito } from "next/font/google";
 import Image from "next/image";
 
 import style from "../styles/Intro.module.css";
-import logo from "../../Public/Art.svg";
+import artwork from "../../Public/Art.svg";
 
 const nunito = Nunito({ subsets: ["latin"], weight: ["500"] });
 
 export default function Intro() {
   return (
     <section className={`${style.Intro} ${nunito.className}`}>
-      <div className={style.details}>
+      <div>
         <p className={style.greet}>Hi! I&apos;m</p>
         <h1 className={style.header}>Ismayel Alam</h1>
         <p className={style.description}>
@@ -18,16 +18,12 @@ export default function Intro() {
           leverage my skills and creativity to build visually stunning and
           user-friendly websites.
         </p>
-        <button className={style.btn}>Get in touch</button>
-        <button className={style.btn}>Download CV</button>
+        <a className={style.btn}>Get in touch</a>
+        <a className={style.btn} href="/ismayel_alam_resume.pdf" download>
+          Download CV
+        </a>
       </div>
-      <Image
-        src={logo}
-        alt="logo"
-        width={700}
-        height={700}
-        className={style.heroImage}
-      />
+      <Image src={artwork} alt="logo" className={style.heroImage} />
     </section>
   );
 }
