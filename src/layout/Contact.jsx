d68f8ca2ac80ee6f26e04d10a128145dfca94afa
Ismayel Alam <ismayelalam0@gmail.com>
2023-08-26 13:35:26 +0600

    create and style the contact section

--- src/layout/Contact.jsx
+++ src/layout/Contact.jsx
@@ -1,10 +1,57 @@
-import SectionHeader from "@/components/SectionHeader";
+import { BsGithub, BsLinkedin, BsSend } from "react-icons/bs";
+import { IoCallOutline, IoMailOutline } from "react-icons/io5";
+
 import style from "../styles/Contact.module.css";
 
 export default function Contact() {
   return (
     <section className={style.contact} id="contact">
-      <SectionHeader title="Contact" />
+      <div className={style.map}>
+        <form action="" className={style.form}>
+          <h4 className={style.heading}>get in touch</h4>
+          <input
+            type="text"
+            required
+            className={style.input}
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            required
+            className={style.input}
+            placeholder="Email"
+          />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className={style.input}
+            placeholder="Message"
+            required
+          ></textarea>
+          <button type="submit" className={style.btn}>
+            <span>send</span>
+            <BsSend />
+          </button>
+        </form>
+        <div className={style.social}>
+          <div className={style.mails}>
+            <p className={style.mail}>
+              <IoMailOutline className={style.icons} />
+              <span> Ismayelalam0@gmail.com</span>
+            </p>
+            <p className={style.mail}>
+              <IoCallOutline className={style.icons} />
+              <span>(+880) 1626 256336</span>
+            </p>
+          </div>
+          <div className={style.links}>
+            <BsLinkedin className={style.icons} />
+            <BsGithub className={style.icons} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
