05f3e95c6d2fd8cf1972da9f134cc94c1597a221
Ismayel Alam <ismayelalam0@gmail.com>
2023-08-26 22:01:22 +0600

    refector code for contact links

---
+++ src/components/ContactLinks.jsx
@@ -0,0 +1,40 @@
+import { BsGithub, BsLinkedin, BsSend } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
+import { IoCallOutline, IoMailOutline } from "react-icons/io5";
+
+import style from "../styles/Contact.module.css";
+
+export default function ContactLinks() {
+  return (
+    <div className={style.links}>
+      <p className={style.link}>
+        <CiLocationOn className={style.icons} />
+        <span> Narayanganj, Bangladesh</span>
+      </p>
+      <a className={style.link} href="mailto:Ismayelalam0@gmail.com">
+        <IoMailOutline className={style.icons} />
+        <span> Ismayelalam0@gmail.com</span>
+      </a>
+      <a className={style.link} href="tel:(+880) 1626 256336">
+        <IoCallOutline className={style.icons} />
+        <span>(+880) 1626 256336</span>
+      </a>
+      <a
+        href="https://github.com/IsmayelAlam"
+        target="_blank"
+        className={style.link}
+      >
+        <BsGithub className={style.icons} />
+        <span href="tel:(+880) 1626 256336">github</span>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ismayelalam/"
+        target="_blank"
+        className={style.link}
+      >
+        <BsLinkedin className={style.icons} />
+        <span>Linkedin</span>
+      </a>
+    </div>
+  );
+}
--- src/layout/Contact.jsx
+++ src/layout/Contact.jsx
@@ -1,10 +1,7 @@
-import { BsGithub, BsLinkedin, BsSend } from "react-icons/bs";
-import { CiLocationOn } from "react-icons/ci";
-import { IoCallOutline, IoMailOutline } from "react-icons/io5";
-
 import style from "../styles/Contact.module.css";
 import Map from "@/components/Map";
 import ContactForm from "@/components/ContactForm";
+import ContactLinks from "@/components/ContactLinks";
 
 export default function Contact() {
   return (
@@ -14,29 +11,7 @@ export default function Contact() {
       <ContactForm />
 
       <div className={style.social}>
-        <div>
-          <p className={style.link}>
-            <CiLocationOn className={style.icons} />
-            <span> Narayanganj, Bangladesh</span>
-          </p>
-          <p className={style.link}>
-            <IoMailOutline className={style.icons} />
-            <a href="mailto:Ismayelalam0@gmail.com"> Ismayelalam0@gmail.com</a>
-          </p>
-          <p className={style.link}>
-            <IoCallOutline className={style.icons} />
-            <a href="tel:(+880) 1626 256336">(+880) 1626 256336</a>
-          </p>
-        </div>
-
-        <div className={style.links}>
-          <a href="https://www.linkedin.com/in/ismayelalam/" target="_blank">
-            <BsLinkedin className={style.icons} />
-          </a>
-          <a href="https://github.com/IsmayelAlam" target="_blank">
-            <BsGithub className={style.icons} />
-          </a>
-        </div>
+        <ContactLinks />
       </div>
     </section>
   );
