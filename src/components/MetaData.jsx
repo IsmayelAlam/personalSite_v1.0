4caaf74b87308dc588c35339ebd90bd27dc796f1
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-01 21:13:28 +0600

    clean up files

--- src/components/MetaData.jsx
+++ src/components/MetaData.jsx
@@ -35,15 +35,16 @@ export default function MetaData() {
       <link
         rel="icon"
         type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
+        sizes="18x18"
+        href="/favicon-18x18.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="18x18"
-        href="/favicon-18x18.png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
       />
+      <link rel="apple-touch-icon" sizes="192x192" href="/apple-icon.png" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -53,16 +54,6 @@ export default function MetaData() {
         rel="apple-touch-icon"
         sizes="152x152"
         href="/apple-icon-152x152.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/apple-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/apple-icon-120x120.png"
       />
 
       <link rel="manifest" href="/site.webmanifest" />
