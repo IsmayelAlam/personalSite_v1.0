84e605d8d37bd18650850e13987b45f6e83bd0e0
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-01 19:36:39 +0600

    add manifest file

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,22 +19,31 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" type="image/png" href="/favicon.ico" />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-16x16.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-32x32.png"
+          href="/favicon-16x16.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+
         <link
           rel="apple-touch-icon"
           sizes="57x57"
@@ -76,22 +85,27 @@ export default function RootLayout({ children }) {
           href="/apple-icon-152x152.png"
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-icon-192x192.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
 
-        <meta name="msapplication-TileColor" content="#ffffff" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="msapplication-TileColor" content="#ffffff0" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff"></meta>
+
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#e0fbfc"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#293241"
+        />
       </head>
       <body className={inter.className}>
         <DarkModeProvider>
